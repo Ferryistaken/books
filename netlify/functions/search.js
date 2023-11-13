7979a4e5cdb74b12e33a99d694dbbd6f6fb980df
Ferry <alessandro.ferrari.2004@gmail.com>
2023-11-13 09:08:45 -0500

    fixed func

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const math = require('mathjs');
+const { pipeline } = require('@huggingface/transformers');
 
 exports.handler = async (event) => {
     // Load embeddings data
-    let data = fs.readFileSync(path.join(__dirname, '../embeddings.json'));
-    data = JSON.parse(data);
+    let rawData = fs.readFileSync(path.join(__dirname, '../embeddings.json'));
+    let data = JSON.parse(rawData);
 
     const sentences = data.sentences;
     const embeddings = data.embeddings;
@@ -13,25 +14,37 @@ exports.handler = async (event) => {
     // Extract query from event
     const query = event.queryStringParameters.q;
 
-    // Calculate query embedding - this part depends on how you implement it in JS
-    const queryEmbedding = ...; // You need to define this part
+    // Initialize the feature extraction pipeline
+    let extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
 
-    // Calculate similarities
-    let scores = embeddings.map(embedding => {
-        return math.dot(queryEmbedding, embedding) / 
-               (math.norm(queryEmbedding) * math.norm(embedding));
+    // Calculate query embedding
+    extractor(query, { pooling: 'mean', normalize: true }).then(queryEmbeddingOutput => {
+        const queryEmbedding = queryEmbeddingOutput[0]; // Extract the embedding
+
+        // Calculate similarities
+        let scores = embeddings.map(embedding => {
+            return math.dot(queryEmbedding, embedding) /
+                   (math.norm(queryEmbedding) * math.norm(embedding));
+        });
+
+        // Find top results
+        let indices = scores.map((score, index) => [score, index]);
+        indices.sort((a, b) => b[0] - a[0]);
+        let topResults = indices.slice(0, 5);
+
+        // Prepare response
+        const results = topResults.map(item => sentences[item[1]]);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results)
+        };
+    }).catch(error => {
+        console.error("Error in processing query:", error);
+        return {
+            statusCode: 500,
+            body: "Error processing your request"
+        };
     });
-
-    // Find top results
-    let indices = scores.map((score, index) => [score, index]);
-    indices.sort((a, b) => b[0] - a[0]);
-    let topResults = indices.slice(0, 5);
-
-    // Prepare response
-    const results = topResults.map(item => sentences[item[1]]);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(results)
-    };
 };
+
