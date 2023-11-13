2e78aa8960e5734cd6f8e671d0250de0cfc019fa
Ferry <alessandro.ferrari.2004@gmail.com>
2023-11-13 09:55:05 -0500

    new function mapping

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -4,9 +4,12 @@ const math = require('mathjs');
 import { pipeline } from '@xenova/transformers';
 
 exports.handler = async (event) => {
-    // Load embeddings data
-    let rawData = fs.readFileSync(path.join(__dirname, '../embeddings.json'));
-    let data = JSON.parse(rawData);
+    // URL of the embeddings JSON file
+    const embeddingsUrl = '<url>/embeddings.json';
+
+    // Fetch the embeddings data from the URL
+    const response = await fetch(embeddingsUrl);
+    const data = await response.json();
 
     const sentences = data.sentences;
     const embeddings = data.embeddings;
@@ -18,33 +21,25 @@ exports.handler = async (event) => {
     let extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
 
     // Calculate query embedding
-    extractor(query, { pooling: 'mean', normalize: true }).then(queryEmbeddingOutput => {
-        const queryEmbedding = queryEmbeddingOutput[0]; // Extract the embedding
+    const queryEmbeddingOutput = await extractor(query, { pooling: 'mean', normalize: true });
+    const queryEmbedding = queryEmbeddingOutput[0]; // Extract the embedding
 
-        // Calculate similarities
-        let scores = embeddings.map(embedding => {
-            return math.dot(queryEmbedding, embedding) /
-                   (math.norm(queryEmbedding) * math.norm(embedding));
-        });
-
-        // Find top results
-        let indices = scores.map((score, index) => [score, index]);
-        indices.sort((a, b) => b[0] - a[0]);
-        let topResults = indices.slice(0, 5);
-
-        // Prepare response
-        const results = topResults.map(item => sentences[item[1]]);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(results)
-        };
-    }).catch(error => {
-        console.error("Error in processing query:", error);
-        return {
-            statusCode: 500,
-            body: "Error processing your request"
-        };
+    // Calculate similarities
+    let scores = embeddings.map(embedding => {
+        return math.dot(queryEmbedding, embedding) / 
+               (math.norm(queryEmbedding) * math.norm(embedding));
     });
-};
 
+    // Find top results
+    let indices = scores.map((score, index) => [score, index]);
+    indices.sort((a, b) => b[0] - a[0]);
+    let topResults = indices.slice(0, 5);
+
+    // Prepare response
+    const results = topResults.map(item => sentences[item[1]]);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results)
+    };
+};
