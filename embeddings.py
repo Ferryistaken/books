3adc783a57e9f96fecc477cbb5338f9fcbeab7a3
Ferry <alessandro.ferrari.2004@gmail.com>
2023-11-17 16:49:29 -0500

    new embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,3 +1,5 @@
+print("Loading libraries")
+
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 import json
@@ -25,9 +27,13 @@ def get_book_title(isbn):
     else:
         return "Title Not Found"
 
+print("Loading Model")
+
 # Load the model
 model = SentenceTransformer("all-MiniLM-L6-v2")
 model.max_seq_length = 256
+
+print("Loading data")
 
 # Read the CSV file
 df = pd.read_csv('sheet.csv')
@@ -43,6 +49,8 @@ sentences = df['highlight'].tolist()
 isbns = df['isbn'].tolist()
 group_indices = df['group_index'].tolist()
 
+print("Encoding data")
+
 # Generate embeddings
 embeddings = model.encode(sentences, normalize_embeddings=True)
 
@@ -54,8 +62,13 @@ data = {
     "isbns": isbns, 
     "group_indices": group_indices
 }
+
+print("Saving embeddings")
+
 with open("embeddings.json", "w") as file:
     json.dump(data, file)
+
+print("Generating visualization")
 
 # Dimensionality reduction using UMAP
 umap_embeddings = umap.UMAP(n_neighbors=15, n_components=2, metric='cosine').fit_transform(embeddings)
@@ -68,18 +81,37 @@ isbn_to_title = {isbn: get_book_title(isbn) for isbn in unique_isbns}
 colors = plt.cm.jet(np.linspace(0, 1, len(unique_isbns)))
 isbn_to_color = {isbn: color for isbn, color in zip(unique_isbns, colors)}
 
+plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
+
 # Plotting
 plt.figure(figsize=(12,10))
+legend_elements = []
+
 for idx, isbn in enumerate(unique_isbns):
     indices = [i for i, x in enumerate(isbns) if x == isbn]
     marker_style = markers[idx % len(markers)]  # Cycle through markers
-    plt.scatter(umap_embeddings[indices, 0], umap_embeddings[indices, 1], s=20, color=isbn_to_color[isbn], label=isbn_to_title[isbn], marker=marker_style)
 
-plt.title('Embeddings Visualization using UMAP')
-plt.xlabel('UMAP Dimension 1')
-plt.ylabel('UMAP Dimension 2')
-plt.legend(title='Book Title', bbox_to_anchor=(1.05, 1), loc='upper left')
+    # Scatter plot
+    scatter = plt.scatter(umap_embeddings[indices, 0], umap_embeddings[indices, 1], s=30, color=isbn_to_color[isbn], marker=marker_style)
+
+    # Calculating centroid of each cluster
+    centroid_x = np.mean(umap_embeddings[indices, 0])
+    centroid_y = np.mean(umap_embeddings[indices, 1])
+
+    # Annotate with book title at the centroid
+    plt.annotate(isbn_to_title[isbn], (centroid_x, centroid_y), fontsize=9, ha='center', va='center', color='white')
+
+    # Create a custom legend entry for each ISBN
+    legend_elements.append(plt.Line2D([0], [0], marker=marker_style, color='w', label=isbn_to_title[isbn], markersize=10, markerfacecolor=scatter.get_facecolor()[0], linestyle='None'))
+
+plt.title('Vector Space (UMAP)', fontsize=20)
+
+# Add a legend and adjust its properties
+legend = plt.legend(handles=legend_elements, title='', loc='upper right', bbox_to_anchor=(1, 0.9), bbox_transform=plt.gcf().transFigure)
+legend.get_frame().set_alpha(0.5)  # Adjust the opacity
+
+plt.grid(False)
+plt.axis('off')
 
 # Save the plot as a PNG file
 plt.savefig("umap.png", dpi=300, bbox_inches='tight')
-
